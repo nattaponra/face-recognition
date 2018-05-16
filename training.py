eaf7cce60850c369b7292640116d71ccb414adbf
nattaponra <nattapon.rakthong@gmail.com>
2018-05-16 23:06:35 +0700

    List file from training_picture folder

--- training.py
+++ training.py
@@ -2,11 +2,15 @@ import face_recognition
 import json
 import pickle
 import FaceRecognition
+import glob
+ 
 #Input
+
 inputPictures = ["unknow_image/12828502_1005842369534847_7136850454606854375_o.jpg",
                  "unknow_image/25438871_1565928383526240_8675585191151944587_o.jpg",
                  "unknow_image/10981161_695207507265003_5104416470050938630_n.jpg",
                  "unknow_image/25358342_1566786296773782_5191830380202574644_o.jpg"]
+inputPictures = glob.glob("training_picture/*.jpg")
 def getEncodePicture(path):
     pictureInput         = face_recognition.load_image_file(path)
     inputPictureEncoding = face_recognition.face_encodings(pictureInput)[0]
